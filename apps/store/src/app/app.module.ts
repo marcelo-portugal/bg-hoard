6d14fe4232512a57803ecbeb15162b04676d0f22
Marcelo Portugal <marcelo.portugal@ukg.com>
2022-10-21 15:32:49 -0400

    add header component by generating a new nx lib (unfortunately with an extra lib folder

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { LibsStoreUiSharedModule } from 'libs/libs/store/ui-shared/src';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
@@ -13,6 +14,7 @@ import { appRoutes } from './app.routes';
     BrowserModule,
     MatCardModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
+    LibsStoreUiSharedModule
   ],
   providers: [],
   bootstrap: [AppComponent],
